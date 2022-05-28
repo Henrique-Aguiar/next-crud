290a32572f2343a523c7b10c4511e598cc7a4dac
Henrique-Aguiar <hrikelr@hotmail.com>
2022-05-27 21:18:34 -0300

    criando tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,59 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
+import Botao from '../components/Botao'
+import Formulario from '../components/Formulario'
 import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 35, '2'),
+    new Cliente('Carlos', 44, '3'),
+    new Cliente('Pedro', 22, '4'),
+
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`excluir ${cliente.nome}`)
+  }
+
+  function salvarCliente(cliente: Cliente) {
+
+  }
+
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
   return (
     <div className={`
-      flex h-screen justify-center items-center
-      bg-gradient-to-r from-blue-500 to-purple-500
-      text-white
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-blue-500 to-purple-500
+    text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        {visivel === 'tabela' ? (
+          <>
+            <div className='flex justify-end'>
+              <Botao cor='green' className='m-4' 
+                onClick={() => setVisivel('form')}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado}
+            clienteExcluido={clienteExcluido}></Tabela>
+          </>
+        ) : (
+          <Formulario 
+            cliente={clientes[1]}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel('tabela')}
+          />
+        )}
       </Layout>
     </div>
   )
